c8154c14600e0bc60bc7ccc691ae5dba4d6b08b1
AFresnedo <AFresnedo@users.noreply.github.com>
2018-09-13 13:21:59 -0700

    finish Meal schema

--- models/Meal.js
+++ models/Meal.js
@@ -2,11 +2,17 @@ let mongoose = require('mongoose');
 
 let mealSchema = new mongoose.Schema({
   name: {
+    type: String,
+    required: true
   },
   prep: {
+    type: Number
   },
-  ingredients: {
-  },
+  ingredients: [{
+    type: String
+  }],
   funny: {
+    type: Boolean,
+    default: false
   }
 });
